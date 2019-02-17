c363b24edfcfab03ee82d3c82dca5731ded86c31
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-02-17 05:23:05 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -43,7 +43,18 @@ class InteractiveRecord
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end 
   
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM #{table_name} WHERE name = ?
+    SQL
+    DB[:conn].execute(sql, name)
+  end 
   
-  
-  
+  def self.find_by(hash)
+    sql = <<-SQL
+      SELECT * FROM #{table_name} WHERE #{hash.keys.join} = ?
+    SQL
+    DB[:conn].execute(sql, hash.values.join)
+  end 
+
 end
